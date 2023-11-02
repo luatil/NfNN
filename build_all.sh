322aec6cfbf522a8ca18057b1a327e1cfbf26acd
Luã Santilli <lua.til@usp.br>
2023-11-02 15:25:53 -0300

    build: Print build messages
    Signed-off-by: Luã Santilli <lua.til@usp.br>

--- build_all.sh
+++ build_all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-opts="-g -Wall -Wno-unused-function -O3"
+opts="-Wall -Wno-unused-function -O3"
 link_ops="-lm"
 includes="lib"
 out_dir="build"
@@ -9,22 +9,22 @@ out_dir="build"
 mkdir -p "$out_dir"
 # cd "$out_dir" || exit
 
-# Build tests
+echo "Build tests"
 gcc $opts -I"$includes" tests/tests.c -o "$out_dir"/tests $link_ops
 
-# Build examples - xor
+echo "Build examples - xor"
 gcc $opts -I"$includes" examples/xor/mlp/src/xor.c -o "$out_dir"/xor $link_ops
 
-# Build examples - mnist dataloader
+echo "Build examples - mnist dataloader"
 gcc $opts -I"$includes" examples/mnist/mnist_single_device/src/mnist_test_dataloader.c -o "$out_dir"/mnist_test_dataloader $link_ops
 
-# Build examples - mnist single device
+echo "Build examples - mnist single device"
 gcc $opts -I"$includes" examples/mnist/mnist_single_device/src/mnist_single_device.c -o "$out_dir"/mnist_single_device $link_ops
 
-# Build examples - mnist sync parameter server
+echo "Build examples - mnist sync parameter server"
 gcc $opts -I"$includes" examples/mnist/mnist_sync_parameter_server/src/mnist_sync_parameter_server.c -o "$out_dir"/mnist_sync_parameter_server $link_ops
 
-# Build examples - mnist async parameter server
+echo "Build examples - mnist async parameter server"
 gcc $opts -I"$includes" examples/mnist/mnist_async_parameter_server/src/mnist_async_parameter_server.c -o "$out_dir"/mnist_async_parameter_server $link_ops
 
 # cd ..
